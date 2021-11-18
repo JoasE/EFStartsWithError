4c5b07db20093a2486b6f9e5525f319550c557b7
JoasE <32096708+JoasE@users.noreply.github.com>
2021-11-18 13:27:00 +0100

    Initial commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new TestContext();
+
+            var test = context.Languages.Where(x => x.Name.StartsWith("t", StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+    }
+}
---
+++ ConsoleApp4/TestContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp4
+{
+    public class Language
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    class TestContext : DbContext
+    {
+        public TestContext() : base(new DbContextOptionsBuilder<TestContext>()
+            .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TestContext").Options)
+        {
+        }
+
+        public DbSet<Language> Languages { get; set; }
+    }
+}
